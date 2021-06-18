b458621c9f2eebed0bad7cb42eee5f52ddf71f96
rafael-ramos-dev <79939946+rafael-ramos-dev@users.noreply.github.com>
2021-06-18 00:12:59 -0300

    17-06-2021- update

--- App.js
+++ App.js
@@ -21,50 +21,6 @@ const styles = StyleSheet.create({
     marginTop: 200,
     marginRight: 20,
     marginLeft: 20
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
 
-// const [pokemonData, setPokemonData] = useState([]);
-  // const [nextUrl, setNextUrl] = useState('');
-  // const [previousUrl, setPreviousUrl] = useState('');
-  // const [loading, setLoading] = useState(true);
-  // const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
-
-  // useEffect(() => {
-  //   async function fecthPokemonData() {
-  //     let responseAllPokemon = await getAllPokemon(initialUrl);
-
-  //     setNextUrl(responseAllPokemon.next);
-  //     setPreviousUrl(responseAllPokemon.previous);
-  //     await loadingPokemon(responseAllPokemon.results);
-  //     setLoading(false);
-  //   }
-  //   fecthPokemonData();
-  // }, []);
-
-  // const loadingPokemon = async data => {
-  //   let _pokemonData = await Promise.all(data.map(async pokemon => {
-  //     let pokemonRecorded = await getPokemon(pokemon.url);
-  //     return pokemonRecorded;
-  //   }));
-
-  //   setPokemonData(_pokemonData);
-  // };
-
- // <View>
-          //   <FlatList
-          //   contentContainerStyle={{paddingBottom: 20}}
-          //   showsVerticalScrollIndicator={false}
-          //   data={(pokemonData)}
-          //   renderItem={({pokemon}) => <Card pokemon={pokemonData} />}
-          //           />
-          // </View>
-          // <Card pokemon={pokemonData[0]} />
-
-          // <View>
-          //   {pokemonData.map((pokemon, i) => {
-          //     return <Card key={i} pokemon={pokemon} />
-          //   })}
-          // </View> 
